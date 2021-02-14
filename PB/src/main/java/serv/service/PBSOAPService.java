8ce0760eb6e78bcd50eb790f45d78b20b44aa38b
Nikita <inozemcevniki@gmail.com>
2021-02-14 10:08:18 +0300

    Add get and post request in js
    +

--- PB/src/main/java/serv/service/PBSOAPService.java
+++ PB/src/main/java/serv/service/PBSOAPService.java
@@ -3,9 +3,7 @@ package serv.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -30,10 +28,10 @@ public class PBSOAPService {
 	 * @param color Команда сервису
 	 * @return Возврат признака успеха
 	 */
-
+	@CrossOrigin
 	@RequestMapping(value = "/update", method = RequestMethod.POST)
 	@ResponseBody
-	public void insert(String color) {
+	public void insert(@RequestBody String color) {
 		basetalker.insert(color);
 	}
 
@@ -41,7 +39,7 @@ public class PBSOAPService {
 	 *
 	 * @return Обновление изображения
 	 */
-
+	@CrossOrigin
 	@RequestMapping(value = "/update", method = RequestMethod.GET)
 	public ResponseEntity<String> select() {
 		return basetalker.select();
